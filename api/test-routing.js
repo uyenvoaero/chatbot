29928f7ea916c5f55ff65aeb69babca4bb6c8870
uyenvoaero <uyenvoaero@gmail.com>
2025-09-12 23:46:35 +0900

    Remove the unnecessary test endpoints

--- api/test-routing.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (req, res) => {
-    res.status(200).json({
-        message: 'Routing test successful',
-        method: req.method,
-        url: req.url,
-        query: req.query,
-        headers: req.headers
-    });
-};
